8bebee25ffebc1dccf41331ec72cc46941528d5a
Nicolas Leiva <nicolasleiva@gmail.com>
2017-04-24 19:58:42 -0400

    Simplified data.go

--- gserver/data.go
+++ gserver/data.go
@@ -5,7 +5,7 @@ Two different assign values to our Routers struct
 package main
 
 import (
-	"fmt"
+	//"fmt"
 	pb "github.com/nleiva/gmessaging/gproto"
 )
 
@@ -25,34 +25,31 @@ var routers1 = []*pb.Router{
 	},
 }
 
-func variables() {
-	//Individual pointers
-	r1 := &pb.Router{
-		Hostname: "router1.cisco.com",
-		IP:       []byte("2001:db8::111:11:1"),
-	}
-	r2 := &pb.Router{
-		Hostname: "router2.cisco.com",
-		IP:       []byte("2001:db8::222:22:2"),
-	}
-	r3 := &pb.Router{
-		Hostname: "router3.cisco.com",
-		IP:       []byte("2001:db8::333:33:3"),
-	}
-
-	// Method 1: Append individual pointers to the Router field of the Routers instance
-	routers2 := pb.Routers{}
-	routers2.Router = append(routers2.Router, r1, r2, r3)
-
-	// Method 2: Create instance type Routers with the list of pointers
-	routers3 := pb.Routers{routers1}
-
-	// Verifying the variable type
-	fmt.Printf("Type global var: %T,\n", routers1)
-	fmt.Printf("Type local instace: %T,\n", routers2)
-
-	// Contents of the two Routers instances
-	fmt.Printf("%v\n", routers2)
-	fmt.Printf("%v\n", routers3)
-
+//Individual pointers
+var r1 = &pb.Router{
+	Hostname: "router1.cisco.com",
+	IP:       []byte("2001:db8::111:11:1"),
 }
+var r2 = &pb.Router{
+	Hostname: "router2.cisco.com",
+	IP:       []byte("2001:db8::222:22:2"),
+}
+var r3 = &pb.Router{
+	Hostname: "router3.cisco.com",
+	IP:       []byte("2001:db8::333:33:3"),
+}
+
+// Method 1: Append individual pointers to the Router field of the Routers instance
+//var routers2 = pb.Routers{}
+//routers2.Router = append(routers2.Router, r1, r2, r3)
+
+// Method 2: Create instance type Routers with the list of pointers
+var routers3 = pb.Routers{routers1}
+
+// Verifying the variable type
+//fmt.Printf("Type global var: %T,\n", routers1)
+//fmt.Printf("Type local instace: %T,\n", routers2)
+
+// Contents of the two Routers instances
+//fmt.Printf("%v\n", routers2)
+//fmt.Printf("%v\n", routers3)
